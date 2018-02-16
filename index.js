97d43b752ecf8bdf8ab178496e41cee8c687cff1
Michael Morris <mdam2345@gmail.com>
2018-02-16 20:47:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var recipes = { prop:"1" , prop2:"2"} ;
   return recipes ;
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  recipes.prop:"1"
+  recipes.prop2:"2"
+  return recipes
+}
